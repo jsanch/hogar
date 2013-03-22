731479416e2625e70eecd2d4e2f09ddfdd6d5c93
jgarciach <jgarciach@gmail.com>
2013-03-22 16:02:02 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hogar::Application.config.secret_token = '57063f15a9fc1e3b051c5e97784207091fcf4ca658442d1a399b876ac57c4ebccb4ac45c03bb318d59e34dd3b37df3aaa4382af0e6444888a88b6200b1da9c19'
