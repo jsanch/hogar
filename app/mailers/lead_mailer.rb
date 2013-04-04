5aae23674151384f046a9321fd8d296f5d5b6939
jgarciach <jgarciach@gmail.com>
2013-04-04 17:17:56 -0500

    added bcc to gaby

--- app/mailers/lead_mailer.rb
+++ app/mailers/lead_mailer.rb
@@ -2,6 +2,6 @@ class LeadMailer < ActionMailer::Base
   default from: "ubiqua@ubiqua.me"
 
   def thank_lead(email)
-    mail(to: email, subject: "test")
+    mail(to: email, subject: "test", bcc: "ghurtado@ubiqua.me")
   end
 end
