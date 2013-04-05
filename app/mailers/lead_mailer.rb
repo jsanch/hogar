67fbdbb97bd6498a1c77e31d15a54fb94be21f69
jgarciach <jgarciach@gmail.com>
2013-04-04 19:48:17 -0500

    Changed subject line

--- app/mailers/lead_mailer.rb
+++ app/mailers/lead_mailer.rb
@@ -2,6 +2,6 @@ class LeadMailer < ActionMailer::Base
   default from: "ubiqua@ubiqua.me"
 
   def thank_lead(email)
-    mail(to: email, subject: "test", bcc: "ghurtado@ubiqua.me")
+    mail(to: email, subject: "Gracias por Contactarnos!", bcc: "ghurtado@ubiqua.me")
   end
 end
